f756bb54e7b51b98a80580cfc983d5c0bba2d23f
Relyte <96859270+Relyte@users.noreply.github.com>
2022-12-22 00:51:08 -0500

    Remove bash script for signing/broadcasting

--- sign_and_broadcast.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in $(ls /tmp/dist*); do
-        echo "Processing $i"
-        secretcli tx sign $i --from slash_refund -ojson --output-document ~/dist_signed.json --node https://secretnetwork-rpc.stakely.io:443 --chain-id secret-4
-        sleep 2
-        secretcli tx broadcast ~/dist_signed.json --node https://secretnetwork-rpc.stakely.io:443 --chain-id secret-4
-done
